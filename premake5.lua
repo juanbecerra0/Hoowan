c1c892de7708ee280a1feac2650c1c102b99943b
juanbecerra0 <47235983+juanbecerra0@users.noreply.github.com>
2020-06-16 16:31:03 -0600

    Fixed XCOPY premake bug

--- premake5.lua
+++ premake5.lua
@@ -70,7 +70,7 @@ project "Hoowan"
 
 		postbuildcommands
 		{
-			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
 		}
 
 	filter "configurations:Debug"
