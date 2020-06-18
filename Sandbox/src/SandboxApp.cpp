9b21131fdf3372e9670862e39fee3cb84d6c5d1b
juanbecerra0 <47235983+juanbecerra0@users.noreply.github.com>
2020-06-18 09:02:48 -0600

    Implemented glm math library

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,11 +1,17 @@
 #include <Hoowan.h>
 
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
+
 class ExampleLayer : public Hoowan::Layer
 {
 public:
 	ExampleLayer()
 		: Layer("Example")
 	{
+
 	}
 
 	void OnUpdate() override
--- premake5.lua
+++ premake5.lua
@@ -17,6 +17,7 @@ IncludeDir = {}
 IncludeDir["GLFW"] = "Hoowan/vendor/GLFW/include"
 IncludeDir["Glad"] = "Hoowan/vendor/Glad/include"
 IncludeDir["ImGui"] = "Hoowan/vendor/imgui"
+IncludeDir["glm"] = "Hoowan/vendor/glm"
 
 include "Hoowan/vendor/GLFW"
 include "Hoowan/vendor/Glad"
@@ -37,7 +38,9 @@ project "Hoowan"
 	files
 	{
 		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/glm/glm/**.hpp",
+		"%{prj.name}/vendor/glm/glm/**.inl"
 	}
 
 	includedirs
@@ -46,7 +49,8 @@ project "Hoowan"
 		"%{prj.name}/vendor/spdlog/include",
 		"%{IncludeDir.GLFW}",
 		"%{IncludeDir.Glad}",
-		"%{IncludeDir.ImGui}"
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}"
 	}
 
 	links 
@@ -106,7 +110,8 @@ project "Sandbox"
 	includedirs
 	{
 		"Hoowan/vendor/spdlog/include",
-		"Hoowan/src"
+		"Hoowan/src",
+		"%{IncludeDir.glm}"
 	}
 
 	links
