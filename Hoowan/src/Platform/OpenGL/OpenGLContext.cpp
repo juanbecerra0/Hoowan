aaca3bd0180ca2477b73be505f7fa93374ab3934
juanbecerra0 <47235983+juanbecerra0@users.noreply.github.com>
2020-06-20 09:28:12 -0600

    Added triangle rendering example

--- Hoowan/src/Platform/OpenGL/OpenGLContext.cpp
+++ Hoowan/src/Platform/OpenGL/OpenGLContext.cpp
@@ -18,6 +18,11 @@ namespace Hoowan {
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		HW_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		HW_CORE_INFO("OpenGL Info:");
+		HW_CORE_INFO("	Vendor: {0}", glGetString(GL_VENDOR));
+		HW_CORE_INFO("	Renderer: {0}", glGetString(GL_RENDERER));
+		HW_CORE_INFO("	Version: {0}", glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
