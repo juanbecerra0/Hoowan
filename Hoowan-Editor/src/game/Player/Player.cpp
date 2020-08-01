b17252f520c2cc0a70ef72c6f787baf5691709c5
juanbecerra0 <47235983+juanbecerra0@users.noreply.github.com>
2020-08-01 13:31:36 -0600

    Implemented gravity and jumping for the player

--- Hoowan-Editor/src/game/Player/Player.cpp
+++ Hoowan-Editor/src/game/Player/Player.cpp
@@ -2,6 +2,7 @@
 #include "Player.h"
 
 #include <glm\ext\matrix_transform.hpp>
+#include <glm/gtx/compatibility.hpp>
 
 Player::Player(Hoowan::Ref<Hoowan::Scene> scene)
 {
@@ -18,31 +19,57 @@ Player::~Player()
 
 void Player::OnUpdate(Hoowan::Timestep ts)
 {
+	// Modify velocity with these methods
 	Move(Hoowan::Input::IsKeyPressed(Hoowan::KeyCode::A), Hoowan::Input::IsKeyPressed(Hoowan::KeyCode::D), ts);
+	Jump(Hoowan::Input::IsKeyPressed(Hoowan::KeyCode::Space), ts);
+	ApplyGravity(ts);
+
+	// Apply transformation
+	glm::mat4& transform = m_PlayerEntity.GetComponent<Hoowan::TransformComponent>().GetTransform();
+	transform = glm::translate(transform, glm::vec3(m_Velocity.x, m_Velocity.y, 0.0f) * ts.GetSeconds());
 }
+
+// Util
 
 void Player::Move(bool holdingLeft, bool holdingRight, Hoowan::Timestep ts)
 {
-	glm::mat4& transform = m_PlayerEntity.GetComponent<Hoowan::TransformComponent>().GetTransform();
-
 	if (holdingLeft)
 	{
-		transform = glm::translate(transform, glm::vec3(-m_Speed, 0, 0) * ts.GetSeconds());
+		m_Velocity.x = -MAX_VELOCITY.x;
 		FlipPlayerOrientation(true);
 	}
 	else if (holdingRight)
 	{
-		transform = glm::translate(transform, glm::vec3(m_Speed, 0, 0) * ts.GetSeconds());
+		m_Velocity.x = MAX_VELOCITY.x;
 		FlipPlayerOrientation(false);
 	}
+	else
+	{
+		m_Velocity.x = 0.0f;
+	}
+}
 
+void Player::Jump(bool holdingJump, Hoowan::Timestep ts)
+{
+	if (holdingJump && !m_HasJumped)
+	{
+		m_HasJumped = true;
 
+		m_Velocity.y = MAX_VELOCITY.y;
+	}
+	else if (!holdingJump)
+	{
+		m_HasJumped = false;
+	}
+}
 
+void Player::ApplyGravity(Hoowan::Timestep ts)
+{
+	m_Velocity.y = std::clamp(m_Velocity.y - (MAX_VELOCITY.y * m_Mass * ts.GetSeconds()), -MAX_VELOCITY.y, MAX_VELOCITY.y);
 }
 
 void Player::FlipPlayerOrientation(bool flip)
 {
 	Hoowan::SubTexture2D& subTexture = m_PlayerEntity.GetComponent<Hoowan::SpriteRendererSubTextureComponent>().GetSubTexture();
-
 	flip ? subTexture.FlipOrientation(false, true) : subTexture.FlipOrientation(false, false);
 }
--- Hoowan-Editor/src/game/Player/Player.h
+++ Hoowan-Editor/src/game/Player/Player.h
@@ -13,6 +13,8 @@ public:
 
 private:
 	void Move(bool holdingLeft, bool holdingRight, Hoowan::Timestep ts);
+	void Jump(bool holdingJump, Hoowan::Timestep ts);
+	void ApplyGravity(Hoowan::Timestep ts);
 
 	void FlipPlayerOrientation(bool flip);
 
@@ -20,6 +22,10 @@ private:
 	Hoowan::Entity m_PlayerEntity;
 	PlayerSprites m_PlayerSprites;
 
-	float m_Speed = 3.0f;
+	glm::vec2 m_Velocity = { 0.0f, 0.0f };
+	const glm::vec2 MAX_VELOCITY = { 5.0f, 9.8f };
 
+private:
+	float m_Mass = 1.8f;
+	bool m_HasJumped = false;
 };
